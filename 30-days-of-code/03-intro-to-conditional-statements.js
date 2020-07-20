5d8f366b910f28ce5501e658a6cfcd31171e1cff
Lucas <lucasdesousa19@hotmail.com>
2020-07-20 14:37:05 -0300

    03 into to conditional statements

---
+++ 30-days-of-code/03-intro-to-conditional-statements.js
@@ -0,0 +1,17 @@
+const n = 3
+
+// console.log(n % 2)
+if(n % 2 !== 0 || n >= 6 &&  n <= 20){
+  console.log('Weird')  
+}else{
+  console.log('Not Weird')
+}
+
+// if(n >= 2 || n < 5) {
+//   console.log('Not Weird')
+// }else if(n >= 6 || n <= 20){
+//   console.log('Weird')
+// }else if(n > 20){
+//   console.log('Not Weird')
+// }
+
